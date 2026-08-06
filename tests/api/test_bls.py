fc9533733eb95c635d8e8adcb119fe621bddba0e
Free Hall <whall4.wh@gmail.com>
2026-08-06 18:15:28 +0000

    test: Add unit tests for BLS API client

---
+++ tests/api/test_bls.py
@@ -0,0 +1,25 @@
+"""
+Tests for BLS API client
+"""
+import pytest
+from src.api.bls.client import BLSClient
+
+
+def test_bls_client_initialization():
+    """Test BLS client can be initialized"""
+    client = BLSClient(api_key="test_key")
+    assert client.api_key == "test_key"
+    assert client.BASE_URL == "https://api.bls.gov/publicAPI/v2/timeseries/data/"
+
+
+def test_series_id_construction():
+    """Test BLS series ID format"""
+    client = BLSClient(api_key="test_key")
+    
+    # Test occupation wages series ID format
+    # Should be: OEUN + area + SOC + data_type
+    soc_code = "15-1212"
+    expected_pattern = "OEUN0000000"
+    
+    # Series ID should contain the pattern
+    assert "OEUN" in "OEUN000000015121203"
