881d55f8e401481fd073331b5787ad0c27a59b36
Free Hall <whall4.wh@gmail.com>
2026-08-10 14:12:50 +0000

    Add Unity Catalog permission grants

---
+++ config/table_grants.sql
@@ -0,0 +1,25 @@
+-- Unity Catalog Permissions for For Your Service
+-- Organization: 7 Eagle Group
+
+-- Grant READ on Bronze table
+GRANT SELECT ON TABLE workspace.fys_bronze.job_postings 
+TO `fys-analysts`;
+
+-- Grant WRITE on Silver table
+GRANT SELECT, INSERT, UPDATE ON TABLE workspace.fys_silver.job_postings_enriched 
+TO `fys-data-engineers`;
+
+-- Grant READ on Gold table
+GRANT SELECT ON TABLE workspace.fys_gold.job_embeddings 
+TO `fys-ml-engineers`;
+
+-- Grant MODIFY on all tables to admin group
+GRANT ALL PRIVILEGES ON SCHEMA workspace.fys_bronze TO `fys-admins`;
+GRANT ALL PRIVILEGES ON SCHEMA workspace.fys_silver TO `fys-admins`;
+GRANT ALL PRIVILEGES ON SCHEMA workspace.fys_gold TO `fys-admins`;
+
+-- Create service principal for automated jobs
+CREATE SERVICE PRINCIPAL IF NOT EXISTS 'fys-job-runner';
+
+GRANT SELECT, INSERT ON TABLE workspace.fys_bronze.job_postings 
+TO SERVICE PRINCIPAL 'fys-job-runner';
