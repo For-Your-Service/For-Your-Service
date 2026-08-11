eb76e675353aa640929b1af34842897f6e60d328
whall4.wh@gmail.com <whall4.wh@gmail.com>
2026-08-11 14:56:23 +0000

    feat(features): Add   init

---
+++ src/matching/features/__init__.py
@@ -0,0 +1,4 @@
+"""Feature extractors."""
+from .skill_features import extract_skill_count, extract_skill_diversity
+from .experience_features import extract_total_experience, extract_role_count
+from .education_features import extract_degree_level
