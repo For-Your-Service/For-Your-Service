09112e311eaea44df59257df8332efde7df041d3
whall4.wh@gmail.com <whall4.wh@gmail.com>
2026-08-11 14:56:17 +0000

    feat(validators): Add   init

---
+++ src/resume_parsing/validators/__init__.py
@@ -0,0 +1,5 @@
+"""Validators module."""
+from .email_validator import validate_email
+from .phone_validator import validate_phone
+from .date_validator import validate_date
+from .url_validator import validate_url
