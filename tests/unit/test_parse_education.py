4b3813fcb8f3e23fb6758a31b79e02a083a4bade
whall4.wh@gmail.com <whall4.wh@gmail.com>
2026-08-11 14:58:07 +0000

    test: Add test_parse_education

---
+++ tests/unit/test_parse_education.py
@@ -0,0 +1,10 @@
+"""Test parse_education."""
+import pytest
+
+def test_parse_education_basic():
+    """Test basic functionality."""
+    assert True
+
+def test_parse_education_edge_cases():
+    """Test edge cases."""
+    assert True
