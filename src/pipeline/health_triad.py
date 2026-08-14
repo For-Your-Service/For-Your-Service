a0a48e7df58b429ba611be27fd4037e60d64dd96
Free Hall <whall4.wh@gmail.com>
2026-08-14 16:56:52 -0500

    feat(pipeline): implement pipeline health triad framework for FYS-109 (#108)

---
+++ src/pipeline/health_triad.py
@@ -0,0 +1,8 @@
+﻿class PipelineHealthTriad:
+    def __init__(self, ingestion_source, build_engine, output_sink):
+        self.in_bound = ingestion_source
+        self.build_engine = build_engine
+        self.out_bound = output_sink
+
+    def verify_triad(self) -> bool:
+        return all([self.in_bound, self.build_engine, self.out_bound])
