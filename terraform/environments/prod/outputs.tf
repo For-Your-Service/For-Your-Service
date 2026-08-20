ff3cb53e8d14a8700becc76c6a0a110349507ce7
Free <whall4.wh@gmail.com>
2026-08-20 05:54:17 -0500

    feat(terraform/env/prod): configure prod outputs, tfvars example, and remote backend template

---
+++ terraform/environments/prod/outputs.tf
@@ -0,0 +1,24 @@
+# File: terraform/environments/prod/outputs.tf
+# Description: Production Environment Outputs
+# Lead Architect: Free Hall <whall4.wh@gmail.com>
+# Organization: 7 Eagle Group
+
+output "aws_s3_data_bucket" {
+  value = var.enable_aws ? module.aws[0].s3_data_bucket_id : null
+}
+
+output "aws_s3_staging_bucket" {
+  value = var.enable_aws ? module.aws[0].s3_staging_bucket_id : null
+}
+
+output "gcp_storage_archive_bucket" {
+  value = var.enable_gcp ? module.gcp[0].archive_bucket_name : null
+}
+
+output "databricks_bronze_schema" {
+  value = var.enable_databricks ? module.databricks[0].bronze_schema_name : null
+}
+
+output "huggingface_space_url" {
+  value = var.enable_huggingface ? module.huggingface[0].space_url : null
+}
