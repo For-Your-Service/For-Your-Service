ece7f31dcff825f92eb71c1e42d231fb57685ea4
Free <whall4.wh@gmail.com>
2026-08-21 10:30:53 -0500

    style(hygiene): sanitize formatting and strip trailing whitespace in config/table_grants.sql

--- config/table_grants.sql
+++ config/table_grants.sql
@@ -2,15 +2,15 @@
 -- Organization: 7 Eagle Group
 
 -- Grant READ on Bronze table
-GRANT SELECT ON TABLE workspace.fys_bronze.job_postings 
+GRANT SELECT ON TABLE workspace.fys_bronze.job_postings
 TO `fys-analysts`;
 
 -- Grant WRITE on Silver table
-GRANT SELECT, INSERT, UPDATE ON TABLE workspace.fys_silver.job_postings_enriched 
+GRANT SELECT, INSERT, UPDATE ON TABLE workspace.fys_silver.job_postings_enriched
 TO `fys-data-engineers`;
 
 -- Grant READ on Gold table
-GRANT SELECT ON TABLE workspace.fys_gold.job_embeddings 
+GRANT SELECT ON TABLE workspace.fys_gold.job_embeddings
 TO `fys-ml-engineers`;
 
 -- Grant MODIFY on all tables to admin group
@@ -21,5 +21,5 @@ GRANT ALL PRIVILEGES ON SCHEMA workspace.fys_gold TO `fys-admins`;
 -- Create service principal for automated jobs
 CREATE SERVICE PRINCIPAL IF NOT EXISTS 'fys-job-runner';
 
-GRANT SELECT, INSERT ON TABLE workspace.fys_bronze.job_postings 
+GRANT SELECT, INSERT ON TABLE workspace.fys_bronze.job_postings
 TO SERVICE PRINCIPAL 'fys-job-runner';
