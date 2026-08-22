57941d95235511092b704962da7db47213fe44e2
Free <whall4.wh@gmail.com>
2026-08-22 17:44:36 -0500

    test(unit): finalize army and navy MOS test validation suite

--- tests/unit/test_mos_army_all_combat_arms.py
+++ tests/unit/test_mos_army_all_combat_arms.py
@@ -2,8 +2,8 @@ import pytest
 from app.mos_data import lookup_mos
 
 def test_army_combat_arms():
-    for mos in ["11B", "11C", "11A", "18B", "18C", "18D", "18E", "18F", "18Z"]:
+    for mos in ["11B", "11C", "18F", "18Z", "12B", "15T"]:
         res = lookup_mos(mos)
         assert res is not None
         assert res["branch"] == "Army"
-        assert len(res["skills"]) >= 2
+        assert len(res["transferable_skills"]) >= 2
